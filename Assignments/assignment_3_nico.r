0c1ad0066cdeba539ac332785e8fcc0620b71a16
nicogavigan <nicogavigan@ucsb.edu>
2026-01-29 09:29:17 -0800

    working on assignments

--- Assignments/assignment_3_nico.r
+++ Assignments/assignment_3_nico.r
@@ -1,25 +1,15 @@
-# Assignment 3
+# Assignment 3: Almond Yield Anomaly
 # Nico Gavigan
 # Jan 21 2026
 # -------------------------------------------
-#' @parameter 
+# CALCULATING ALMOND YIELD ANOMALY
+# -------------------------------------------
 
-
-
-#Y = -0.015Tn,2 - 0.0046T2n,2 - 0:07P,1 + 0.0043P^2,1 + 0.28
-
-# Y: Yield anomaly (ton/acre) - departure from the expected/average yield, not total yield 
-# Tn,2 : minimum monthly temperature for Feb.
-# P1 = precipitation for January
-
-
-# LOAD LIBRARIES 
-library(tidyverse)
-library(here)
-
-# LOAD DATA
-clim <- read_table(here("Assignments", "clim.txt")) |>
-  janitor::clean_names()
+# Equation:
+#         Y = -0.015Tn - 0.0046Tn^2 - 0:07P + 0.0043P^2 + 0.28
+# Y   : yield anomaly (ton/acre) - departure from the expected/average yield, not total yield 
+# Tn  : average minimum February temperature
+# P   : total January precipitation 
 
 # INPUTS: daily time series containing
 #   daily tmin
@@ -31,55 +21,64 @@ clim <- read_table(here("Assignments", "clim.txt")) |>
 #   min yield anomaly
 #   mean yield anomaly
 
+# -------------------------------------------
+# LOAD LIBRARIES 
+library(tidyverse)
+library(here)
 
-# STEP 1: CONVERT DAILY INTO THE SPECIFIC MONTHLY METRICS THE EQUATION REQUIRES
-# PRE-PROCESS DATA
+# LOAD DATA
+clim <- read_table(here("Assignments", "clim.txt")) |>
+  janitor::clean_names()
 
-# CREATE DATAFRAME FOR FEBRUARY MEAN MINIMUM TEMPERATURE (tmin_c)
-Tn2 <- clim |>
+# -------------------------------------------
+# STEP 1: PRE-PROCESS DATA
+
+# CALCULATE FEBRUARY MEAN MINIMUM TEMPERATURE (tmin_c)
+temp <- clim |>
   filter(month == 2) |>
   group_by(year) |>
-  summarize(avg_tmin_c = mean(tmin_c))
+  summarize(avg_tmin_c = mean(tmin_c)) |>
+  pull(avg_tmin_c)
 
 # CREATE DATAFRAME FOR TOTAL JANUARY PRECIPITATION (sum_precip)
-P1 <- clim |>
+precip <- clim |>
   filter(month == 1) |>
   group_by(year) |>
-  summarize(sum_precip_mm = sum(precip))
-
-# BIND TABLES TO CREATE ONE DATAFRAME OF SELECTED CLIMATE VARIABLES
-clim_var <- cbind.data.frame(P1, avg_tmin_c = Tn2$avg_tmin_c)
+  summarize(sum_precip_mm = sum(precip)) |>
+  pull(sum_precip_mm)
 
 
-# STEP 2: CREATE A FUNCTION TO CALCULATE ALMOND YIELD ANOMOLY
-
-
-# WORKING ALMOND MODEL FUNCTION
-
-almond_model <- function(Tn2, P1, B1 = -0.015, B2 = -0.0046, 
-                         B3 = -0.07, B4 = 0.0043, B0 = 0.28){
+# STEP 2: CREATE A FUNCTION TO CALCULATE MIN, MAX, MEAN YIELD ANOMOLY
+almond_model <- function(T, P, year = NULL, B1 = -0.015, B2 = -0.0046, 
+                         B3 = -0.07, B4 = 0.0043, B0 = 0.28,
+                         return_type = c("summary", "yearly")){
   
-  Y = B1*Tn2 + B2*Tn2^2 + B3*P1 + B4*P1^2 + B0
+  return_type <- match.arg(return_type)
   
-  return(Y)
+  Y = B1*T + B2*T^2 + B3*P + B4*P^2 + B0
+  
+  # SET RETURN TO YIELD MIN, MAX, MEAN AND YEARLY DISTRIBUTION OF ANOMALIES
+  if(return_type == "summary") {
+    return(data.frame(
+      statistic = c("min", "max", "mean"),
+      value = c(min(Y), max(Y), mean(Y))
+    ))
+  }
+  if(!is.null(year)){
+    return(data.frame(year = year, Ya = Y))
+  } else{
+    return(Y)
+  }
 }
 
-almond_model(Tn2 = Tn2, P1 = P1, B1 = -0.015, B2 = -0.0046, 
-             B3 = -0.07, B4 = 0.0043, B0 = 0.28)
+# STEP 3: RUN MODEL
+years <- clim |>
+  distinct(year) |>
+  arrange(year) |>
+  filter(year != "1988") |>
+  pull(year)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+yearly_Ya <- almond_model(T = temp, P = precip, year = years, return_type = "yearly")
 
 
